1b89d8606aa1052e603da97e19bf6b4a47876edb
abdikeem <abdihakimabdirahman21@gmail.com>
2022-04-18 22:13:31 +0300

    .\man3_printf

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
+#include <stdarg.h>
 
 int _putchar(char);
 int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
 
 #endif
