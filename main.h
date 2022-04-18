19106744a765e9060a2fcafa7520428abb3bf178
muhabeid <muhabeid@gmail.com>
2022-04-18 22:03:50 +0300

    Main file

--- main.h
+++ main.h
@@ -1,7 +1,17 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
+#include <stdarg.h>
 
-int _putchar(char);
+typedef struct Cspecs
+{
+char cs;
+void (*f)();
+} cs_t;
+
 int _printf(const char *format, ...);
+void _putchar_c(char c);
+void _putchar(va_list a);
+void print_str(va_list a);
+char *inttobinary(va_list list);
 
 #endif
